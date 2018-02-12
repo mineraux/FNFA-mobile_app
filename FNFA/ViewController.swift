e8b4d2e73d335780e1a3f465d674d83d29455128
MINERVINI Robin <rminervini@g-a101-19.local>
2018-02-12 15:40:42 +0100

    Initial Commit

---
+++ FNFA/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FNFA
+//
+//  Created by MINERVINI Robin on 12/02/2018.
+//  Copyright © 2018 MINERVINI Robin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
