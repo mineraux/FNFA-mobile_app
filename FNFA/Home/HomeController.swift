7cec5e055e16cb33a5bf934de764970a2d0378fc
Alexandre Massé <alexandre.masse94@gmail.com>
2018-02-13 17:18:19 +0100

    homeCollectionView : first experiment

---
+++ FNFA/Home/HomeController.swift
@@ -0,0 +1,42 @@
+//
+//  HomeController.swift
+//  FNFA
+//
+//  Created by MASSE Alexandre on 13/02/2018.
+//  Copyright © 2018 MINERVINI Robin. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewContoller: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource  {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+        
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "homeCell", for: indexPath) as! HomeCollectionViewCell
+        
+        cell.label.text = "test"
+        return cell
+    }
+
+}
