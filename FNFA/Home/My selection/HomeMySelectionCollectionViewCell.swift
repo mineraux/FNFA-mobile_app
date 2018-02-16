5649144d54a921a5529c795e4b7887833b971b77
Alexandre Massé <alexandre.masse94@gmail.com>
2018-02-16 01:23:51 +0100

    Home : new structure with TableView and container view

--- FNFA/Home/My selection/HomeMySelectionCollectionViewCell.swift
+++ FNFA/Home/My selection/HomeMySelectionCollectionViewCell.swift
@@ -1,22 +1,19 @@
 //
-//  HomeCollectionViewCell.swift
+//  HomeMySelectionCollectionViewCell.swift
 //  FNFA
 //
-//  Created by MASSE Alexandre on 13/02/2018.
+//  Created by Alexandre Massé on 16/02/2018.
 //  Copyright © 2018 MINERVINI Robin. All rights reserved.
 //
 
 import UIKit
 
-class HomeCollectionViewCell: UICollectionViewCell{
-
+class HomeMySelectionCollectionViewCell: UICollectionViewCell {
+    
     @IBOutlet weak var eventCategory: UILabel!
-
     @IBOutlet weak var eventName: UILabel!
-
-    
     @IBOutlet weak var eventPlace: UILabel!
-    @IBOutlet weak var eventDate: UILabel!
-    
     @IBOutlet weak var eventImage: UIImageView!
+
+    @IBOutlet weak var eventDate: UILabel!
 }
