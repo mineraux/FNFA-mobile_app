0e445ad9f4019928de7cf29c12a153fd4fe84f1d
MINERVINI Robin <rminervini@g-a101-19.local>
2018-03-15 10:32:43 +0100

    Tab bar styles

---
+++ FNFA/TabBarController.swift
@@ -0,0 +1,40 @@
+//
+//  TabBarController.swift
+//  FNFA
+//
+//  Created by MINERVINI Robin on 15/03/2018.
+//  Copyright © 2018 MINERVINI Robin. All rights reserved.
+//
+
+import UIKit
+
+class TabBarController: UITabBarController {
+    
+
+    let kBarHeight = 80
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
