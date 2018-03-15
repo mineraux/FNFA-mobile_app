74f4f6a5aed45cc8c36bef6231212b679f12a7d4
Robin Minervini <robin.minervini@gmail.com>
2018-03-15 23:15:46 +0100

    map

--- FNFA/aboutController.swift
+++ FNFA/aboutController.swift
@@ -7,8 +7,10 @@
 //
 
 import UIKit
+import CoreLocation
+import MapKit
 
-class aboutController: UITableViewController {
+class aboutController: UITableViewController, CLLocationManagerDelegate, MKMapViewDelegate {
 
     @IBOutlet weak var containerIntro: UIView!
     @IBOutlet weak var containerRestauration: UIView!
@@ -22,6 +24,9 @@ class aboutController: UITableViewController {
     @IBOutlet weak var square6: UIView!
     @IBOutlet weak var square7: UIView!
     @IBOutlet weak var square8: UIView!
+    
+    @IBOutlet weak var mapView: MKMapView!
+    let locationManager = CLLocationManager()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -45,6 +50,29 @@ class aboutController: UITableViewController {
         square8.layer.cornerRadius = 4
         
         tableView.allowsSelection = false;
+        
+        mapView.delegate = self
+        mapView.showsUserLocation = true
+        locationManager.delegate = self
+        
+        let span:MKCoordinateSpan = MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1);
+        let mapCenterView:CLLocationCoordinate2D = CLLocationCoordinate2DMake(48.117056, -1.678498)
+        let region:MKCoordinateRegion = MKCoordinateRegionMake(mapCenterView, span)
+        mapView.setRegion(region, animated: true)
+        
+        addAnnotation(name: "TNB", latitute: 48.10796740000001, longitude: -1.6725616999999602)
+        addAnnotation(name: "Arvor", latitute: 48.1159954, longitude: -1.6791138999999475)
+        addAnnotation(name: "ESRA", latitute: 48.1288092, longitude: -1.6420402999999624)
+        addAnnotation(name: "Grand Logis", latitute: 48.0231681, longitude: -1.7446339000000535)
+        addAnnotation(name: "Esplanade Charles de Gaulles ", latitute: 48.1064896, longitude: -1.6767549999999574)
+    }
+    
+    func addAnnotation(name:String!, latitute:Double!, longitude:Double!) -> Void {
+        let annotation = MKPointAnnotation()
+        let location:CLLocationCoordinate2D = CLLocationCoordinate2DMake(latitute, longitude)
+        annotation.coordinate = location
+        annotation.title = name
+        return mapView.addAnnotation(annotation)
     }
     
     override func didReceiveMemoryWarning() {
