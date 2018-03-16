271a26604ed929a73643d82cd4a24c81a47b7edb
MINERVINI Robin <rminervini@g-a101-19.local>
2018-03-16 19:55:35 +0100

    try fix favorites

--- FNFA/Home/Tomorrow/HomeTomorrowViewController.swift
+++ FNFA/Home/Tomorrow/HomeTomorrowViewController.swift
@@ -99,7 +99,6 @@ class HomeTomorrowViewController: UIViewController,  UICollectionViewDelegate, U
         
         favIconeManager(indexPath: indexPath, addToFavBtn: tomorrowCell.favButton)
         
-        
         return  tomorrowCell
     }
     
